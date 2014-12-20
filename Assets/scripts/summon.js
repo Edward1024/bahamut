1d0e95bc39f20326f90cda80b091c95f3972f291
Edward1024 <edward791024@gmail.com>
2014-12-20 16:47:38 +0800

    角色生成、敵方塔扣血

---
+++ Assets/scripts/summon.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var id :int;
+var prefab : GameObject;
+
+function SummonMonster () {
+	Instantiate(prefab, GameObject.Find("mySpawnPoint").transform.position, Quaternion.identity);
+}
